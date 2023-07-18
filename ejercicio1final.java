9872e3c3c2cb156273c7320bc29b8f46bcd69e56
Tobiascorbett25 <136532498+Tobiascorbett25@users.noreply.github.com>
2023-07-18 04:10:01 -0300

    Add files via upload

---
+++ ejercicio1final.java
@@ -0,0 +1,32 @@
+package Final1;
+import java.util.Scanner;
+public class ejercicio1final {
+   //numeros primos//
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        int n = scanner.nextInt();
+        boolean esPrimo = true;
+        if(n<=1) {
+            esPrimo = false;
+        }else if(n==2){
+            esPrimo = true;
+        }else
+
+            for (int i = 2; i <n; i++) {
+                if (n % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+                esPrimo = true;
+            }
+
+        if (esPrimo == true) {
+            System.out.println(n + " es un número primo.");
+        } else {
+            System.out.println(n + " NO es un número primo.");
+        }
+    }
+}
+
